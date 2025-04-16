9f2930bf51e594382ade70218f9b626ea6c79355
muzammil <muzammilshaik55@proton.me>
2025-04-16 11:54:53 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,8 +3,8 @@ terraform {
     bucket         = "terraform-state-secsys-bucket"
     key            = "simpletimeservice/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    #use_lockfile  = true
+    #dynamodb_table = "terraform-locks"
+    use_lockfile  = true
     encrypt        = true
   }
 }
