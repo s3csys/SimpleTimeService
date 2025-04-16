870bb4daa321a7ea2763d65809f8b2c053aade02
muzammil <muzammilshaik55@proton.me>
2025-04-16 12:57:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,8 +129,8 @@ resource "aws_security_group" "ecs_service_sg" {
   vpc_id = aws_vpc.my_vpc.id
 
   ingress {
-    from_port   = var.container_port
-    to_port     = var.container_port
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -245,7 +245,7 @@ resource "aws_lb_target_group" "ecs_tg" {
 # ALB Listener
 resource "aws_lb_listener" "ecs_listener" {
   load_balancer_arn = aws_lb.ecs_alb.arn
-  port              = var.container_port
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
